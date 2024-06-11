88e334c4ec6699cfcafd80f2d77e1dfd8c11138c
Doug Scheible <doug_scheible@yahoo.com>
2024-06-11 16:44:42 -0500

    src and styles started for sections

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Section from "./components/Section";
+import Footer from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Section />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
