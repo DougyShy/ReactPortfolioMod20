8c337e3727c135a074f18e8ae9c2e0cc64715159
Doug Scheible <doug_scheible@yahoo.com>
2024-06-12 21:29:00 -0500

    navbar/header links working

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -6,7 +6,7 @@ import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Resume');
+  const [currentPage, setCurrentPage] = useState('About');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
