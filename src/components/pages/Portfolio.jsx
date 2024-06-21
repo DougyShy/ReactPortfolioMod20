c9dfdb3720340c1887a45419fde472da76ad0912
Doug Scheible <doug_scheible@yahoo.com>
2024-06-20 20:39:08 -0500

    Portfolio links working. Now just need to add correct links and background images

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -21,14 +21,53 @@ export default function Portfolio() {
   return (
     <div style={styles.portfolioStyle} className="px-5 py-3">
       <h1>Portfolios</h1>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <img className="portfolio-image" src='src/components/images/smartMirror.png' />
-
+        <div className="d-flex flex-row flex-wrap align-items-center p-3 justify-content-around">
+          <div className="image-container">
+            <img className="portfolio-image" src="src\components\images\password.png" />
+            <div className="overlay">
+              <div className="text text-decoration-none"><a href="https://dougyshy.github.io/PasswordGenerator/" target="_blank" rel="noreferrer noopener">Password Generator</a></div>
+               <img className="github-img" />
+            </div>  
+          </div>
+          <div className="image-container">
+            <img className="portfolio-image" src="src\components\images\logoMaker.png" />
+            <div className="overlay">
+            <div className="text text-decoration-none"><a href="https://www.youtube.com/watch?v=QmciF-lAsyY" target="_blank" rel="noreferrer noopener">Simple Logo Maker</a></div>
+               <img className="github-img" />
+            </div>  
+          </div>
+          <div className="image-container">
+            <img className="portfolio-image" src="src\components\images\smartMirror.png" />
+            <div className="overlay">
+              <div className="text">Your Text Here</div>
+               <img className="github-img" />
+            </div>  
           </div>
         </div>
-      </div>
+
+        <div className="d-flex flex-row flex-wrap align-items-center p-3 justify-content-around">
+          <div className="image-container">
+            <img className="portfolio-image" src="src\components\images\smartMirror.png" />
+            <div className="overlay">
+              <div className="text">Your Text Here</div>
+               <img className="github-img" />
+            </div>  
+          </div>
+          <div className="image-container">
+            <img className="portfolio-image" src="src\components\images\smartMirror.png" />
+            <div className="overlay">
+              <div className="text">Your Text Here</div>
+               <img className="github-img" />
+            </div>  
+          </div>
+          <div className="image-container">
+            <img className="portfolio-image" src="src\components\images\smartMirror.png" />
+            <div className="overlay">
+              <div className="text">Your Text Here</div>
+               <img className="github-img" />
+            </div>  
+          </div>
+        </div>
     </div>
 
   );
