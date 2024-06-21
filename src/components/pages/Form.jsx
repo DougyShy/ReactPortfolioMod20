46a7ed4cc21b590dec79698ab821eb69033100c3
Doug Scheible <doug_scheible@yahoo.com>
2024-06-21 01:02:04 -0500

    Contact template up, small changes need to be made

--- src/components/pages/Form.jsx
+++ src/components/pages/Form.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './style.css';
+import '../../styles/Contact.css';
 
 // Here we import a helper function that will check if the email is valid
 import { checkPassword, validateEmail } from '../utils/helpers.js';
